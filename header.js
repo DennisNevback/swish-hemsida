52f385662b660dd10266b3d30feac58bc0a76623
DueWeb <keviin.sundberg@gmail.com>
2023-05-24 11:43:53 +0200

    Base

---
+++ header.js
@@ -0,0 +1,11 @@
+const headerContent = `
+<h1>DevOps</h1>
+<nav>
+  <ul>
+    <li><a href="/">Hem</a></li>
+    <li><a href="/">Funktion 1</a></li>
+    <li><a href="/">Funktion 2</a></li>
+    <li><a href="/">Funktion 3</a></li>
+  </ul>
+</nav>
+`;
