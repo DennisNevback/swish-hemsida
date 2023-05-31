8e89defa6bd5a4959bb8215db02c5aceff1f3956
DueWeb <keviin.sundberg@gmail.com>
2023-05-31 12:06:17 +0200

    Mainpage stuff transaltion remake v.2 sequels

---
+++ src/mainpage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../path/to/style.css';
+import '../path/to/styleMainPage.css';
+
+function App() {
+    return (
+        <>
+            <header></header>
+            <main>
+                <div className="mainPageContainer">
+                    <img src="../images/mainPageLogo.png" className="mainPageLogo" alt="Main Page Logo" />
+                    <p className="slogan">Collaborate<br />Innovate<br />Excel</p>
+                    <div className="learnMoreContainer">
+                        <button type="button" className="learnMoreButton">Learn More</button>
+                    </div>
+                    <div className="rectangle">
+                        <p className="introText">Welcome to SmartOps <br /> Your trusted DevOps Partner <br /> <br />Founded in 2023 - SmartOps strive to inspire more productive and flexible workflows</p>
+                    </div>
+                </div>
+            </main>
+            <footer></footer>
+        </>
+    );
+}
+
+export default App;
