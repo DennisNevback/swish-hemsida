3a789e314164603519bf768cd9afaed420853951
DennisNevback <dennis.nevback@gmail.com>
2023-05-31 12:06:46 +0200

    updated jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="mainPageContainer">
+        <img src="../images/mainPageLogo.png" class="mainPageLogo"></img>
+        <p class="slogan">Collaborate
+          Innovate
+          Excel</p>
+        <div className="learnMoreContainer">
+          <button type="button" class="learnMoreButton">Learn More</button>
+        </div>
+        <div className="rectangle">
+          <p classname="introText">Welcome to SmartOps
+            Your trusted DevOps Partner
+            Founded in 2023 - SmartOps strive to inspire more productive and flexible workflows</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
