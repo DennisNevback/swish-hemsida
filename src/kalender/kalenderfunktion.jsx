ad0b44f05968051a2d9dce17cd7c8a75b7e9bde1
DueWeb <keviin.sundberg@gmail.com>
2023-06-02 13:29:26 +0200

    Tried fixing template

---
+++ src/kalender/kalenderfunktion.jsx
@@ -0,0 +1,14 @@
+// calendar.js
+// Här kan du lägga till JavaScript-kod för att hantera kalendern
+
+// Exempel på en funktion för att visa kalendervyn
+function showCalendar() {
+    // Kod för att visa kalendervyn
+}
+
+// Exempel på en funktion för att växla till en annan månad i kalendervyn
+function changeMonth() {
+    // Kod för att hantera byte av månad i kalendervyn
+}
+
+// Annan kod och funktioner för att hantera kalendern kan läggas till här
---
+++ src/kalender/todos.jsx
@@ -0,0 +1,19 @@
+// todos.js
+// Här kan du lägga till JavaScript-kod för att hantera todos
+
+// Exempel på en funktion för att lägga till en todo
+function addTodo() {
+    // Kod för att hantera tillägg av en todo
+}
+
+// Exempel på en funktion för att ta bort en todo
+function removeTodo() {
+    // Kod för att hantera borttagning av en todo
+}
+
+// Exempel på en funktion för att ändra en todo
+function updateTodo() {
+    // Kod för att hantera uppdatering av en todo
+}
+
+// Annan kod och funktioner för att hantera todos kan läggas till här
