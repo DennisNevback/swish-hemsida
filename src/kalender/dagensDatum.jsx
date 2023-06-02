3d52420862f9c75512a957b69b31b08fd0904dcb
DueWeb <keviin.sundberg@gmail.com>
2023-06-02 14:15:29 +0200

    Some changes

--- src/kalender/dagensDatum.jsx
+++ src/kalender/dagensDatum.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+// today.js
+// Här kan du lägga till JavaScript-kod för att hantera dagens datum och tid
+
+import React, { useEffect } from 'react';
 
 function DagensDatum() {
-    // Exempel på en funktion för att visa dagens datum och tid
+    useEffect(() => {
+        showCurrentDateTime();
+    }, []);
+
     function showCurrentDateTime() {
-        // Kod för att visa dagens datum och tid
+        const today = new Date();
+        const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        const timeOptions = { hour: 'numeric', minute: 'numeric', second: 'numeric' };
+
+        const dateElement = document.getElementById('current-date');
+        const timeElement = document.getElementById('current-time');
+
+        dateElement.textContent = today.toLocaleDateString('sv-SE', dateOptions);
+        timeElement.textContent = today.toLocaleTimeString('sv-SE', timeOptions);
     }
 
     return (
         <div>
-            {/* Annan kod och funktioner för att hantera dagens datum och tid kan läggas till här */}
+            {/* Här kan du lägga till JSX-element för att visa dagens datum och tid */}
+            <p id="current-date"></p>
+            <p id="current-time"></p>
         </div>
     );
 }
 
-export default DagensDatum;
+export default DagensDatum
