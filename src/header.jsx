c19fcff4634b212b456c70f64dd326f469798efb
DennisNevback <dennis.nevback@gmail.com>
2023-06-05 19:53:41 +0200

    Added burger menu

--- src/header.jsx
+++ src/header.jsx
@@ -1,20 +1,42 @@
 import React from 'react';
 import './style.css'
+import { useState } from 'react';
+
+
 
 function Header() {
+
+  var [x, setX] = useState(document.getElementsByClassName("menuItems"));
+
+  function myFunction() {
+    setX = document.getElementsByClassName("menuItems");
+    if (x[0].style.display === "none") {
+      x[0].style.display = "block";
+    } else {
+      x[0].style.display = "none";
+    }
+  }
+
+
   return (
     <>
       <nav>
-        <ul>
+        <ul className='menuNav'>
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
           <div class="loggaNav">
             <img src="../images/loggaHeader.png"></img>
             <h1>DevOps</h1>
           </div>
-          <li><a href="/">Services</a></li>
-          <li><a href="/">Our Team</a></li>
-          <li><a href="/">Clients</a></li>
-          <li><a href="/">About us</a></li>
-          <li><a href="/">Contact us</a></li>
+          <div className="menuItems">
+            <li><a href="/">Services</a></li>
+            <li><a href="/">Our Team</a></li>
+            <li><a href="/">Clients</a></li>
+            <li><a href="/">About us</a></li>
+            <li><a href="/">Contact us</a></li>
+          </div>
+          <a href="javascript:void(0);" className="icon" onClick={myFunction}>
+            <i class="fa fa-bars"></i>
+          </a>
         </ul>
       </nav>
     </>
