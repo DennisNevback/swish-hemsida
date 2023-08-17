6f56e9e87ed458d59bfc9d82c82f0d89dcc7464f
DennisNevback <dennis.nevback@gmail.com>
2023-08-17 09:01:33 +0200

    Adding one task functioning

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,35 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+
+  beforeEach(() => {
+    cy.visit('http://localhost:5173/')
   })
+
+  it('Adding a new task', () => {
+    cy.contains('Add Task').click()
+    cy.get('.todo-list').contains('Do dishes').should('exist')
+  })
+
+  /*it('Adding a new task', () => {
+    cy.window().then(($win) => {
+      cy.stub($win, 'prompt').returns('Diska')
+      cy.contains('Add Task').click()
+    })
+    cy.get('.todo-list').contains('Diska').should('exist')
+  })
+
+  it('Adding multiple tasks', () => {
+    cy.window().then(($win) => {
+      cy.stub($win, 'prompt').returns('Diska')
+      cy.contains('Add Task').click()
+    })
+
+    cy.window().then(($win2) => {
+      cy.stub($win2, 'prompt').returns('Diska')
+      cy.contains('Add Task').click()
+    })
+
+    cy.get('.todo-list').contains('Diska')
+  })*/
+
+
 })
