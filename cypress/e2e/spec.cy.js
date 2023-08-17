8155e0defd33328a7a80378e9d583e3c805d0754
DennisNevback <dennis.nevback@gmail.com>
2023-08-17 10:07:53 +0200

    Adding multiple tasks working

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,32 +4,19 @@ describe('template spec', () => {
     cy.visit('http://localhost:5173/')
   })
 
-  it('Adding a new task', () => {
-    cy.contains('Add Task').click()
-    cy.get('.todo-list').contains('Do dishes').should('exist')
-  })
-
-  /*it('Adding a new task', () => {
+  it('Adding multiple new tasks', () => {
     cy.window().then(($win) => {
       cy.stub($win, 'prompt').returns('Diska')
-      cy.contains('Add Task').click()
+      cy.get('.day').first().contains('Add Task').click()
+      cy.get('.day').first().contains('Add Task').click()
+      cy.get('.day').last().contains('Add Task').click()
     })
     cy.get('.todo-list').contains('Diska').should('exist')
+    cy.get('.day').last().click()
+    cy.get('.todo-list').contains('Diska').should('exist')
+    cy.get('.task-count').first().contains('2')
+    cy.get('.task-count').last().contains('1')
   })
-
-  it('Adding multiple tasks', () => {
-    cy.window().then(($win) => {
-      cy.stub($win, 'prompt').returns('Diska')
-      cy.contains('Add Task').click()
-    })
-
-    cy.window().then(($win2) => {
-      cy.stub($win2, 'prompt').returns('Diska')
-      cy.contains('Add Task').click()
-    })
-
-    cy.get('.todo-list').contains('Diska')
-  })*/
 
 
 })
