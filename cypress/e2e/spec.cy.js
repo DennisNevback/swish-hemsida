c37f77ecf725813b6eb54086170b7b6958e333f6
Kevin Sundberg <keviin.sundberg@gmail.com>
2023-08-17 18:26:37 +0200

    NOT FINISHED, error when running edit test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -33,7 +33,22 @@ describe('template spec', () => {
     cy.get('.todo-list').contains('Diska').should('exist')
     cy.get('.task-count').first().contains('1')
     cy.get('.task-count').last().contains('1')
-
   })
+ it('Edit task', () => {
+  cy.window().then(($win) => {
+    const promptStub = cy.stub($win, 'prompt');
 
-})
+    cy.get('.day').first().contains('Add Task').click();
+    cy.get('.day').first().contains('Add Task').click();
+    cy.get('.day').last().contains('Add Task').click();
+    cy.get('.day').last().contains('Add Task').click();
+    cy.get('.day').first().click();
+
+    cy.get('.todo-list ul li').first().contains('Edit').click();
+    promptStub.onFirstCall().returns('Dansa'); // First edit
+
+    cy.get('.todo-list ul li').last().contains('Edit').click();
+    promptStub.onSecondCall().returns('Göra mat'); // Second edit
+  });
+});
+});
