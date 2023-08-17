5778a6d452521150e2071318ed1b1aeb6d3e1845
Kevin Sundberg <keviin.sundberg@gmail.com>
2023-08-17 10:30:21 +0200

    Delete test works

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,33 +3,37 @@ describe('template spec', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5173/')
   })
-
-  it('Adding a new task', () => {
-    cy.contains('Add Task').click()
-    cy.get('.todo-list').contains('Do dishes').should('exist')
-  })
-
-  /*it('Adding a new task', () => {
+  it('Adding multiple new tasks', () => {
     cy.window().then(($win) => {
       cy.stub($win, 'prompt').returns('Diska')
-      cy.contains('Add Task').click()
+      cy.get('.day').first().contains('Add Task').click()
+      cy.get('.day').first().contains('Add Task').click()
+      cy.get('.day').last().contains('Add Task').click()
     })
     cy.get('.todo-list').contains('Diska').should('exist')
+    cy.get('.day').last().click()
+    cy.get('.todo-list').contains('Diska').should('exist')
+    cy.get('.task-count').first().contains('2')
+    cy.get('.task-count').last().contains('1')
   })
-
-  it('Adding multiple tasks', () => {
+  it('Delete tasks', () => {
     cy.window().then(($win) => {
       cy.stub($win, 'prompt').returns('Diska')
-      cy.contains('Add Task').click()
+      cy.get('.day').first().contains('Add Task').click()
+      cy.get('.day').first().contains('Add Task').click()
+      cy.get('.day').last().contains('Add Task').click()
+      cy.get('.day').last().contains('Add Task').click()
+      cy.get('.day').first().click()
+      cy.get('.todo-list ul li').first().contains('Delete').click()
+      cy.get('.day').last().click()
+      cy.get('.todo-list ul li').last().contains('Delete').click()
     })
+    cy.get('.todo-list').contains('Diska').should('exist')
+    cy.get('.day').last().click()
+    cy.get('.todo-list').contains('Diska').should('exist')
+    cy.get('.task-count').first().contains('1')
+    cy.get('.task-count').last().contains('1')
 
-    cy.window().then(($win2) => {
-      cy.stub($win2, 'prompt').returns('Diska')
-      cy.contains('Add Task').click()
-    })
-
-    cy.get('.todo-list').contains('Diska')
-  })*/
-
+  })
 
 })
